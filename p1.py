1aebfeb0e476c9327ecb3c7b11b4df790ddb01e4
Fabian Mendez <ofmendez@unal.edu.co>
2017-10-01 02:27:31 +0000

    Practica 1, cambio menor agregado -case- a palabras reservadas

--- p1.py
+++ p1.py
@@ -7,8 +7,33 @@ def isDigit(symbol):
 def isAlphaNumeric(symbol):
     return isAlphabet(symbol) or isDigit(symbol)
     
-operators = { "{" :"token_llave_izq", "}" :"token_llave_der", "$" :"token_dollar", ";" :"token_pyc", "[" :"token_cor_izq", "]" :"token_cor_der", "(" :"token_par_izq", ")" :"token_par_der", ">" :"token_mayor", "<" :"token_menor", "!" :"token_not", "+" :"token_mas", "-" :"token_menos", "*" :"token_mul", "/" :"token_div", "%" :"token_mod", "eq" :"token_igual_str", "ne" :"token_diff_str", ">=" :"token_mayor_igual", "<=" :"token_menor_igual", "==" :"token_igual_num", "!=" :"token_diff_num", "&&" :"token_and", "||" :"token_or", "**" :"token_pot"}
-reserved =  {"after":"after"  , "append":"append"  , "array":"array"  , "auto_execok":"auto_execok"  , "auto_import":"auto_import"  , "auto_load":"auto_load"  , "auto_load_index":"auto_load_index"  , "auto_qualify":"auto_qualify"  , "binary":"binary"  , "Bgerror":"Bgerror"  , "break":"break"  , "catch":"catch"  , "cd":"cd"  , "Clock":"Clock"  , "close":"close"  , "concat":"concat"  , "continue":"continue"  , "Dde":"Dde"  , "default":"default"  , "else":"else"  , "elseif":"elseif"  , "Encoding":"Encoding"  , "eof":"eof"  , "error":"error"  , "eval":"eval"  , "Exec":"Exec"  , "exit":"exit"  , "expr":"expr"  , "fblocked":"fblocked"  , "Fconfigure":"Fconfigure"  , "fcopy":"fcopy"  , "file":"file"  , "fileevent":"fileevent"  , "Flush":"Flush"  , "for":"for"  , "foreach":"foreach"  , "format":"format"  , "Gets":"Gets"  , "glob":"glob"  , "global":"global"  , "history":"history"  , "if":"if"  , "info":"info"  , "interp":"interp"  , "join":"join"  , "Lappend":"Lappend"  , "lindex":"lindex"  , "linsert":"linsert"  , "list":"list"  , "Llength":"Llength"  , "load":"load"  , "lrange":"lrange"  , "lreplace":"lreplace"  , "Lsearch":"Lsearch"  , "lsort":"lsort"  , "namespace":"namespace"  , "open":"open"  , "Package":"Package"  , "pid":"pid"  , "pkg_mkIndex":"pkg_mkIndex"  , "proc":"proc"  , "puts":"puts"  , "pwd":"pwd"  , "read":"read"  , "regexp":"regexp"  , "Regsub":"Regsub"  , "rename":"rename"  , "resource":"resource"  , "return":"return"  , "Scan":"Scan"  , "seek":"seek"  , "set":"set"  , "socket":"socket"  , "Source":"Source"  , "split":"split"  , "string":"string"  , "subst":"subst"  , "switch":"switch"  , "tclLog":"tclLog"  , "tell":"tell"  , "time":"time"  , "Trace":"Trace"  , "unknown":"unknown"  , "unset":"unset"  , "update":"update"  , "Uplevel":"Uplevel"  , "upvar":"upvar"  , "vwait":"vwait"  , "while":"while"  , "exists":"exists"  , "then":"then"  }
+operators = { "{" :"token_llave_izq", 
+"}" :"token_llave_der", 
+"$" :"token_dollar", 
+";" :"token_pyc", 
+"[" :"token_cor_izq", 
+"]" :"token_cor_der", 
+"(" :"token_par_izq", 
+")" :"token_par_der", 
+">" :"token_mayor", 
+"<" :"token_menor", 
+"!" :"token_not", 
+"+" :"token_mas", 
+"-" :"token_menos", 
+"*" :"token_mul", 
+"/" :"token_div", 
+"%" :"token_mod", 
+"eq" :"token_igual_str", 
+"ne" :"token_diff_str", 
+">=" :"token_mayor_igual", 
+"<=" :"token_menor_igual", 
+"==" :"token_igual_num", 
+"!=" :"token_diff_num", 
+"&&" :"token_and", 
+"||" :"token_or", 
+"**" :"token_pot"}
+reserved =  {
+    "stdin":"stdin", "gets":"gets", "size":"size", "incr":"incr" , "append":"append"  , "array":"array"  , "auto_execok":"auto_execok"  , "auto_import":"auto_import"  , "auto_load":"auto_load"  , "auto_load_index":"auto_load_index"  , "auto_qualify":"auto_qualify"  , "binary":"binary"  , "Bgerror":"Bgerror"  , "break":"break"  , "catch":"catch"  , "cd":"cd"  , "Clock":"Clock"  , "close":"close"  , "concat":"concat"  , "continue":"continue"  , "Dde":"Dde"  , "default":"default"  , "else":"else"  , "elseif":"elseif"  , "Encoding":"Encoding"  , "eof":"eof"  , "error":"error"  , "eval":"eval"  , "Exec":"Exec"  , "exit":"exit"  , "expr":"expr"  , "fblocked":"fblocked"  , "Fconfigure":"Fconfigure"  , "fcopy":"fcopy"  , "file":"file"  , "fileevent":"fileevent"  , "Flush":"Flush"  , "for":"for"  , "foreach":"foreach"  , "format":"format"  , "Gets":"Gets"  , "glob":"glob"  , "global":"global"  , "history":"history"  , "if":"if"  , "info":"info"  , "interp":"interp"  , "join":"join"  , "Lappend":"Lappend"  , "lindex":"lindex"  , "linsert":"linsert"  , "list":"list"  , "Llength":"Llength"  , "load":"load"  , "lrange":"lrange"  , "lreplace":"lreplace"  , "Lsearch":"Lsearch"  , "lsort":"lsort"  , "namespace":"namespace"  , "open":"open"  , "Package":"Package"  , "pid":"pid"  , "pkg_mkIndex":"pkg_mkIndex"  , "proc":"proc"  , "puts":"puts"  , "pwd":"pwd"  , "read":"read"  , "regexp":"regexp"  , "Regsub":"Regsub"  , "rename":"rename"  , "resource":"resource"  , "return":"return"  , "Scan":"Scan"  , "seek":"seek"  , "set":"set"  , "socket":"socket"  , "Source":"Source"  , "split":"split"  , "string":"string"  , "subst":"subst"  , "switch":"switch"  , "tclLog":"tclLog"  , "tell":"tell"   , "Trace":"Trace"  , "unknown":"unknown"  , "unset":"unset"  , "update":"update"  , "Uplevel":"Uplevel"  , "upvar":"upvar"  , "vwait":"vwait"  , "while":"while"  , "exists":"exists"  , "then":"then" , "case":"case" }
 # reserved["variable"] = "variable" ???
 class DFA:
     def __init__(self,  delta, initial, finals):
@@ -45,7 +70,7 @@ def delta_num(actual, symbol):
     if (symbol == '.'):
         return { 1:3, 4:5 }.get(actual, 0)
     if isDigit(symbol):
-        return { 0:1, 1:1, 3:4, 4:4 }.get(actual, 0)
+        return { 0:1, 1:1, 2:1, 3:4, 4:4 }.get(actual, 0)
     return { 1:2, 4:5 }.get(actual, 0)
 def delta_id(actual, symbol):
     if (isAlphabet(symbol) or symbol == '_'):
@@ -58,20 +83,21 @@ def delta_id(actual, symbol):
 def EvalWordToPrnt(word):
     firstItem = word            if word in reserved  else  "id,{}".format(word)
     firstItem = operators[word] if word in operators else  firstItem
-    print >> f, "<{},{},{}>".format(firstItem,nLine,iniToken)
+    print  "<{},{},{}>".format(firstItem,nLine,iniToken)
 def LaunchError(a,b):
-    print >> f, ">>> Error lexico (linea: {}, posicion: {})".format(a, b )
+    print  ">>> Error lexico (linea: {}, posicion: {})".format(a, b )
     exit()
 def PutAndClear( outStr ):
-    print >> f, outStr
+    print  outStr
     return []
-def MustSaveStrInit(strInit, symbol):
-    return strInit and symbol == '"'
+def MustSaveStrInit(strInit, symbol, strAccept):
+    return (strInit and symbol == '"') or (strAccept and symbol == '"')
     
 ################################# -- MAIN  -- ###################
-f = open('out.txt', 'w')
+# f = open('out.txt', 'w')
 nLine = 0
 mBuffer = []
+lastChar = ' '
 strPos =[1,1]
 
 dfaNumeric   = DFA( delta_num , 0 , {2,5} )
@@ -91,9 +117,10 @@ for line in stdin:
         y += 1
             
         symbolToBuffer = symbol != ' ' and symbol != '"'and symbol != '\n' and symbol != '\r'
-        strPos = [nLine,y] if MustSaveStrInit( dfaStrings.InInit(), symbol) else strPos
+        strPos = [nLine,y] if MustSaveStrInit( dfaStrings.InInit(), symbol, dfaStrings.InAccepting()) else strPos
         dfaStrings.EvalSymbol(symbol)
         
+        # print  "    sym: ", symbol if symbol != '\n' else '\\n'," stat:", dfaStrings.actual , "w:", ''.join(mBuffer),"i:",iniToken
         if ( dfaStrings.InInit() ): ## FUERA DE STRING (SIN INICIAR STRING)
             word = ''.join(mBuffer)
             lastOperatorState = dfaOperators.actual
@@ -101,7 +128,6 @@ for line in stdin:
             dfaNumeric.EvalSymbol(symbol)
             dfaAlphaNum.EvalSymbol(symbol)
             dfaOperators.EvalSymbol(symbol)
-            # print >> f, "sym: ", symbol if symbol != '\n' else '\\n'," stat:", dfaNumeric.actual , "w:", ''.join(mBuffer),"i:",iniToken
             if symbol == '#': ## SOLO APLICA FUERA DE UN STRING
                 break
             elif  dfaAlphaNum.InAccepting():
@@ -139,11 +165,16 @@ for line in stdin:
         elif not symbolToBuffer and symbol != '"' and (symbol != '\n'and symbol != '\r') : ## LEYENDO DENTRO DE STRING
             mBuffer.append(symbol )
             
-        if symbolToBuffer  :
+        if symbolToBuffer  : 
+            if len(mBuffer) >0 and mBuffer[0] in operators and lastChar == '"' :
+                mBuffer =  PutAndClear( "<{},{},{}>".format(operators[mBuffer[0]],nLine,y-2  ) )
+            if dfaStrings.InInit() and symbol not in operators and not isDigit(symbol) and not isAlphaNumeric(symbol) and symbol != '_'and symbol != '|'and symbol != '&' and symbol != '=' and symbol != '.' and symbol != '#':
+                LaunchError(nLine, y)
             mBuffer.append(symbol )
+        lastChar = symbol
             
-if not (dfaStrings.InAccepting() or dfaStrings.InInit() ) : 
-    LaunchError(strPos[0],strPos[1]) # Nunca se cierra un string
+    if not (dfaStrings.InAccepting() or dfaStrings.InInit() ) : 
+        LaunchError(strPos[0],strPos[1]) # Nunca se cierra un string
     
-f.close()
+# f.close()
 
--- sh
+++ sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-for i in $(find ./test -name  "in*"); do # Not recommended, will break on whitespace
-    entrada=${i##*/}
-    salidaEsp='out'${i##*/in}
-    python p1.py < "$i"
-    diffs=$( comm -3  <(sort ./test/$salidaEsp) <(sort out.txt) )
-    if [[ $diffs ]]; then
-        printf   "************* DIF EN $entrada :  \n\n"
-        comm -3  <(sort ./test/$salidaEsp) <(sort out.txt) 
-        printf "\n********************.....\n"
-    else
-        echo "### $entrada ## PASS"
-    fi
-done
+# for i in $(find ./E -name  "in*"); do # Not recommended, will break on whitespace
+#     entrada=${i##*/}
+#     salidaEsp='out'${i##*/in}
+#     python p1.py < "$i"
+#     diffs=$( comm -3  <(sort ./E/$salidaEsp) <(sort out.txt) )
+#     if [[ $diffs ]]; then
+#         printf   "************* DIF EN $entrada :  \n\n"
+#         comm -3  <(sort ./E/$salidaEsp) <(sort out.txt) 
+#         printf "\n********************.....\n"
+#     else
+#         echo "### $entrada ## PASS"
+#     fi
+# done
 printf "\n  _MAIN_ \n"
 python p1.py < ./in.in
-cat out.txt
+# cat out.txt
